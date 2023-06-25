8833ab1f4c4d7eb965065a64fe7c3d43833bd54e
samya-elmain <93472667+samya-elmain@users.noreply.github.com>
2023-06-25 21:28:04 +0100

    Create main.tf

---
+++ backend/main.tf
@@ -0,0 +1,53 @@
+# Define Terraform provider
+terraform {
+  required_version = ">= 1.3"
+  backend "azurerm" {
+    resource_group_name  = "terraform"
+    storage_account_name = "terraformremoteback"
+    container_name       = "tfstate"
+    key                  = "actions.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      version = "~>3.2"
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "azresume-challenge-grp"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "azresumechallengestorage"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "azresume-app-service-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "azresume-linux-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
