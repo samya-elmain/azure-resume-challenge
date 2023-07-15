2d2762a2d41efe03b6248aaa526fcbea82f20748
samya-elmain <93472667+samya-elmain@users.noreply.github.com>
2023-07-15 13:12:00 +0100

    Update main.tf

--- backend/main.tf
+++ backend/main.tf
@@ -47,30 +47,3 @@ resource "azurerm_linux_function_app" "example" {
 
   site_config {}
 }
-resource "azurerm_function_app_function" "example" {
-  name            = "function"
-  function_app_id = azurerm_linux_function_app.example.id
-  language        = "Python"
-  test_data = jsonencode({
-    "name" = "Azure"
-  })
-  config_json = jsonencode({
-    "bindings" = [
-      {
-        "authLevel" = "function"
-        "direction" = "in"
-        "methods" = [
-          "get",
-          "post",
-        ]
-        "name" = "req"
-        "type" = "httpTrigger"
-      },
-      {
-        "direction" = "out"
-        "name"      = "$return"
-        "type"      = "http"
-      },
-    ]
-  })
-}
